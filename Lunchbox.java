8cdb0add949182127eedee6ea5e4452e0d6ec6ce
Liana <liana.hockin@triosstudent.com>
2026-04-03 16:02:09 -0400

    Lunchbox.java

--- Lunchbox.java
+++ Lunchbox.java
@@ -5,13 +5,8 @@ private int quantity;
 private double price;
 private boolean isDelivered;
 
-public Lunchbox() { // create the default constructor first
-    this.customerName = ""; // set everything to zero or empty since setters will change the values later, we need to start fresh
-    this.mainItem = "";
-    this.quantity = 0;
-    this.price = 0.0;
-    this.isDelivered = false;
-}
+public Lunchbox() {} // create the default constructor first
+
  // constructor with parameters
 public Lunchbox(String customerName, String mainItem, int quantity, double price) { 
     this.customerName = customerName;
@@ -22,6 +17,53 @@ public Lunchbox(String customerName, String mainItem, int quantity, double price
 }
 // next I need to create the getters 
 public String getCustomerName() { 
-    return customerName;
+    return this.customerName;
+}
+public String getMainItem() {
+    return this.mainItem;
+}
+public int getQuantity() {
+    return this.quantity;
+}
+public double getPrice() {
+    return this.price;
+}
+public boolean getIsDelivered() {
+    return this.isDelivered;
+}
+// now I need to create the setters
+public void setCustomerName(String customerName) {
+    this.customerName = customerName;
+}
+public void setMainItem(String mainItem) {
+    this.mainItem = mainItem;
+}
+public void setQuantity(int quantity) {
+    this.quantity = quantity;
+}
+public void setPrice(double price) {
+    this.price = price;
+}
+public void setIsDelivered(boolean isDelivered) {
+    this.isDelivered = isDelivered;
+}
+
+// next method is to calculate the cost of the lunchbox
+public double calculateCost() { // use double since its an int being used and it can be a decimal number
+    return this.quantity * this.price; // the amount of items times the price of each item
+}
+// method to mark the lunchbox as delivered
+public void markAsDelivered() { 
+    this.isDelivered = true; // set the isDelivered variable to true instead of false
+}
+//now I need to create the method to display the order 
+public void displayOrder() {
+    System.out.println("Customer Name: " + this.customerName);
+    System.out.println("Main Item: " + this.mainItem);
+    System.out.println("Quantity: " + this.quantity);
+    System.out.println("Price per Item: $" + this.price);
+    System.out.println("Total Cost: $" + this.calculateCost()); 
+    System.out.println("Delivered: " + this.isDelivered);
+}
 
 }
