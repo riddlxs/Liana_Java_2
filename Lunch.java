96e6731faa6a1b2fa0119939d54137dff86bfb5f
Liana <liana.hockin@triosstudent.com>
2026-04-03 16:17:47 -0400

    Lunch.java array

--- Lunch.java
+++ Lunch.java
@@ -1,1 +1,12 @@
 import java.util.Scanner; // import the scanner so it can read the user input!
+
+import java.security.SecureRandom; // import secure random to pick the random dessert
+
+//now create the lunch class that will be the driver class
+public class Lunch { 
+public static void main(String[] args) { // start the program
+    Scanner scanner = new Scanner(System.in); // create the scanner 
+    SecureRandom random = new SecureRandom(); // create the secure random for the dessert!
+Lunchbox[] orders = new Lunchbox[10]; // create the array and set it to 10 orders
+
+}
